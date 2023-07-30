8876196b8398ae5a0a6281c4f1f97653905deaf5
Shivam Tyagi <shivamtyagivivoy55l@gmail.com>
2023-07-30 14:06:47 +0530

    weatherapphosting

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const hbs = require('hbs');
 const app = express();
 const port  = 8000;
 
-const staticpath = path.join(__dirname,"../public");
-const template_path = path.join(__dirname,"../templates/views");
-const partials_path = path.join(__dirname,"../templates/partials")
+const staticpath = path.join(__dirname,"./public");
+const template_path = path.join(__dirname,"./src/templates/views");
+const partials_path = path.join(__dirname,"./src/templates/partials")
 
 
 app.set('view engine','hbs');
